7d1b4a69558ac53e1f69230d3bf4d362bb99c119
Tong Lin <tonglin.t91@gmail.com>
2016-09-22 15:37:15 -0400

    First, and in this case, final commit

--- Tableviews_Part_1/Actor.swift
+++ Tableviews_Part_1/Actor.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+internal struct Actor {
+    internal var firstName: String = ""
+    internal var lastName: String = ""
+    
+    init(from data: String) {
+        let nameComponets: [String] = data.components(separatedBy: " ")
+        
+        if let firstName: String = nameComponets.first,
+            let lastName: String = nameComponets.last{
+            self.firstName = firstName
+            self.lastName = lastName
+        } else {
+            self.firstName = "unset"
+            self.lastName = "unset"
+        }
+    }
+}
